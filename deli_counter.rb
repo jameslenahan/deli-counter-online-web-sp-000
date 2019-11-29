ac2d843f9674d893abb0eecca28238f56c91bdfa
James Lenahan <james.k.lenahan@gmail.com>
2019-11-29 17:53:11 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,12 +1,5 @@
 katz_deli = []
-def line(x)
- line = []
- if (x.length == 0)
-   puts("The line is currently empty.")
-  elsif x.length == 2 
-    
-   puts("")
-  end
-end
+def line(katz_deli)
+  i = 
   
     
